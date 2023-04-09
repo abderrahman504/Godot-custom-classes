9ac0983ed289fab8778953ee12d54c7bcabe81c0
abderrahman504 <rahmankhsherb@gmail.com>
2023-04-10 01:21:16 +0200

    Allow drawer to open either to the outside of parent or the inside

--- UI/DrawerContainer.gd
+++ UI/DrawerContainer.gd
@@ -10,9 +10,10 @@ class_name DrawerContainer
 #For simplicity, postpone drag mode and hide child control on drawer close
 
 
-enum AttachDirection {NORTH, SOUTH, EAST, WEST}
+enum Direction {NORTH, SOUTH, EAST, WEST}
 var handle: BaseButton
-export (AttachDirection) var attachmentDirection: int 
+export (Direction) var openToDirection: int
+export (bool) var attachFromInside: bool
 export (Vector2) var handleSize: Vector2 = Vector2(70, 30)
 export (bool) var isOpen: bool
 var moving: bool = false
@@ -21,14 +22,15 @@ var moving: bool = false
 func _init():
 	handle = Button.new()
 	handle.icon = load("res://icon.png")
-	handle.toggle_mode = true
+	handle.toggle_mode = false
 	handle.icon_align = Button.ALIGN_CENTER
 	handle.expand_icon = true
 	add_child(handle)
 	handle.connect("button_up", self, "on_handle_pressed")
 
 func _ready():
-	adjust_position()
+	call_deferred("adjust_position", false)
+	show_behind_parent = not attachFromInside
 	
 
 
@@ -40,56 +42,84 @@ func _notification(what):
 #		fit_child_in_rect(c, Rect2(Vector2.ZERO, rect_size))
 	
 	var handlePosition: Vector2 = Vector2(0.5*rect_size.x, 0.5*rect_size.y)
-	match attachmentDirection:
-		AttachDirection.SOUTH:
+	match openToDirection:
+		Direction.NORTH:
 			handlePosition.y -= handleSize.y + 0.5*rect_size.y
 			handlePosition.x -= 0.5*handleSize.x
-		AttachDirection.NORTH:
+		Direction.SOUTH:
 			handlePosition.y += 0.5*rect_size.y
 			handlePosition.x -= 0.5*handleSize.x
-		AttachDirection.WEST:
+		Direction.EAST:
 			handlePosition.x += 0.5*rect_size.x
 			handlePosition.y -= 0.5*handleSize.y
-		_:#EAST
+		_:#WEST
 			handlePosition.x -= 0.5*rect_size.x + handleSize.x
 			handlePosition.y -= 0.5*handleSize.y 
 	fit_child_in_rect(handle, Rect2(handlePosition, handleSize))
 
 
-func adjust_position() -> void:
+func adjust_position(smoothTrans: bool) -> void:
 	if moving: return
 	var parent: Control = get_parent()
 	var targetPosition: Vector2
+	var parentBottomY := parent.rect_size.y
+	var parentTopY := 0
+	var parentLeftX := 0
+	var parentRightX := parent.rect_size.x
 	
-	match attachmentDirection:
-		AttachDirection.NORTH:
-			var parentBottomY: float = parent.rect_size.y #Bottom left
-			if isOpen:
-				targetPosition = Vector2(rect_position.x, parentBottomY)
-			else:
-				targetPosition = Vector2(rect_position.x, parentBottomY - rect_size.y)
-			
-		AttachDirection.SOUTH:
-			var parentTopY := 0
-			if isOpen:
-				targetPosition = Vector2(rect_position.x, parentTopY - rect_size.y)
-			else:
-				targetPosition = Vector2(rect_position.x, parentTopY)
-			
-		AttachDirection.EAST:
-			var parentLeftX := 0
-			if isOpen:
-				targetPosition = Vector2(parentLeftX - rect_size.x, rect_position.y)
-			else:
-				targetPosition = Vector2(parentLeftX, rect_position.y)
-			
-		_:#WEST
-			var parentRightX := parent.rect_size.x
-			if isOpen:
-				targetPosition = Vector2(parentRightX, rect_position.y)
-			else:
-				targetPosition = Vector2(parentRightX - rect_size.x, rect_position.y)
 	
+	match openToDirection:
+		Direction.SOUTH:
+			if attachFromInside:
+				if isOpen:
+					targetPosition = Vector2(rect_position.x, parentTopY)
+				else:
+					targetPosition = Vector2(rect_position.x, parentTopY - rect_size.y)
+			else:
+				if isOpen:
+					targetPosition = Vector2(rect_position.x, parentBottomY)
+				else:
+					targetPosition = Vector2(rect_position.x, parentBottomY - rect_size.y)
+			
+		Direction.NORTH:
+			if attachFromInside:
+				if isOpen:
+					targetPosition = Vector2(rect_position.x, parentBottomY - rect_size.y)
+				else:
+					targetPosition = Vector2(rect_position.x, parentBottomY)
+			else:
+				if isOpen:
+					targetPosition = Vector2(rect_position.x, parentTopY - rect_size.y)
+				else:
+					targetPosition = Vector2(rect_position.x, parentTopY)
+			
+		Direction.WEST:
+			if attachFromInside:
+				if isOpen:
+					targetPosition = Vector2(parentRightX - rect_size.x, rect_position.y)
+				else:
+					targetPosition = Vector2(parentRightX, rect_position.y)
+			else:
+				if isOpen:
+					targetPosition = Vector2(parentLeftX - rect_size.x, rect_position.y)
+				else:
+					targetPosition = Vector2(parentLeftX, rect_position.y)
+			
+		_:#EAST
+			if attachFromInside:
+				if isOpen:
+					targetPosition = Vector2(parentLeftX, rect_position.y)
+				else:
+					targetPosition = Vector2(parentLeftX - rect_size.x, rect_position.y)
+			else:
+				if isOpen:
+					targetPosition = Vector2(parentRightX, rect_position.y)
+				else:
+					targetPosition = Vector2(parentRightX - rect_size.x, rect_position.y)
+	
+	if not smoothTrans:
+		set_position(targetPosition)
+		return
 	var tween := create_tween()
 	tween.tween_method(self, "set_position", rect_position, targetPosition, 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	moving = true
@@ -101,8 +131,8 @@ func on_handle_pressed() -> void:
 
 func open() -> void:
 	isOpen = true
-	adjust_position()
+	adjust_position(true)
 
 func close() -> void:
 	isOpen = false
-	adjust_position()
+	adjust_position(true)
